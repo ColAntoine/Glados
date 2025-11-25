7110de209e91f9bad7d3cedecd742319580e3dd5
sumerinsuyu <paul1.emeriau@epitech.eu>
2025-11-25 14:26:19 +0100

    [FEAT]: removed old tests going to build the new ones according to the testing policy

--- test/Test.hs
+++ test/Test.hs
@@ -1,14 +1,11 @@
-import Test.Hspec
-import System.Process (readProcess)
-import Control.Exception (catch, SomeException)
+module Main where
+
+import Test.Tasty
+import qualified LispCases.Call.Call as CallTest
 
 main :: IO ()
-main = hspec $ do
-  describe "Scheme Interpreter" $ do
-    it "should evaluate (if #t 1 2) to 1" $ do
-      schemeCode <- readFile "test/cases/if1.scm"
-      result <- catch (readProcess "glados" ["-lisp"] schemeCode) handleError
-      result `shouldBe` "1\n"
-
-handleError :: SomeException -> IO String
-handleError e = return $ "Error: " ++ show e
+main = do
+  callTests <- CallTest.tests
+  defaultMain $ testGroup "Glados Tests"
+    [ callTests
+    ]
